a2d967a452ad7f5c09d19a04f734b601c813f87c
ATHULYA-123 <62889191+ATHULYA-123@users.noreply.github.com>
2020-03-31 15:57:30 +0530

    Add files via upload

---
+++ Assignment 1a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void main()
+{
+  printf("\nWelcome to learn to code\n");
+  
+}
---
+++ Assignment 1b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+void main()
+{
+  printf("\nName:Athulya Mohan\nDepartment:Ec\nCollege:CEC\n");
+  
+}
