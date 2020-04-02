c55931a7eb649f64ec3101c5ba40b3d19b9c998b
ATHULYA-123 <62889191+ATHULYA-123@users.noreply.github.com>
2020-04-02 09:58:10 +0530

    Add files via upload

---
+++ Assignment 2a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+  char a[]="Athulya",b[]="St joseph HSS";
+  int age=18,marks=100;
+  printf("My name is  %s\n",a);
+    printf("School is %s\n",b);
+    printf("Age is %d\n",age);
+  printf("Marks obtained is %d\n",marks);
+  
+} 
---
+++ Assignment 2b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+  char a[]="Griffindor",b[]="Meat";
+  int age=30;
+  printf("My unicorns's name is  %s\n",a);
+    printf(" Its age is %d\n",age);
+  printf("Favourie food  is %s\n",b);
+  
+} 
