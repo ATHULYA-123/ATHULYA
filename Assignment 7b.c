93c86bd6e1d9017cd21d47aec5777825cc2d6e80
Athulya <62889191+ATHULYA-123@users.noreply.github.com>
2020-04-11 00:15:28 +0530

    Update Assignment 7b.c

--- Assignment 7b.c
+++ Assignment 7b.c
@@ -1,12 +1,19 @@
+#include<stdio.h>
 #include<string.h>
-void main()
+void main ()
 {
-    char a[20];
-    int i,n=0;
-    printf("Enter the string :\n");
-    gets(a);
-    for(i=0;a[i]!='\0';i++)
-    if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
-    {n++;}
-    printf("%d",n);
+  char a[20];
+  int i, n = 0;
+  printf ("Enter the string :\n");
+  gets (a);
+  for (i = 0; a[i] != '\0'; i++)
+    if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o'
+	|| a[i] == 'u')
+      n++;
+    else if (a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O'
+	     || a[i] == 'U')
+      n++;
+    else
+      continue;
+  printf ("%d", n);
 }
