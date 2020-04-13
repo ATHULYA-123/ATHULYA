487d06cb63e6f7bee799d3861ab7a1d157840886
Athulya <62889191+ATHULYA-123@users.noreply.github.com>
2020-04-13 12:21:20 +0530

    Create Assignment 8c.c

---
+++ Assignment 8c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+void
+main ()
+{
+  char a[50], b[50];
+  int i, j, d, c;
+  d = strlen (a);
+  printf ("Enter the string: ");
+  scanf ("%s", a);
+  for (c = 0; c < d; c++)
+    {
+      for (i = 0; i < d - c; i++)
+	{
+	  printf ("\n");
+	  for (j = 0; j <= i; j++)
+	    {
+	      b[j + c] = a[j + c];
+
+	      printf ("%c", b[j + c]);
+	    }
+	}
+    }
+}
