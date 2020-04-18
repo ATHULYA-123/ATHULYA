6b955113be13916fef7e313aa00991506c48d197
Athulya <62889191+ATHULYA-123@users.noreply.github.com>
2020-04-18 10:59:17 +0530

    Create Assignment 10a.c

---
+++ Assignment 10a.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int prime(int n)
+{
+int i;
+if(n==1 || n==0)
+printf("\nNumber is neither prime nor composite.");
+else
+{
+for(i=2;i<n-1;i++)
+  { 
+  if(n%i==0)
+ {
+  printf("Number is composite.\n");
+  return 0;
+ }
+  }
+printf("Number is prime.\n");
+}return 0;
+}
+int main()
+{
+int a;
+printf("Enter number: ");
+scanf("%d",&a);
+prime(a);
+}
