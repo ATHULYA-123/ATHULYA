a6c701b39c68119910cb524860c695bcf3499625
Athulya <62889191+ATHULYA-123@users.noreply.github.com>
2020-04-18 11:53:49 +0530

    Create Assignment 10b.c

---
+++ Assignment 10b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fact(n)
+{
+  int j, f=1,sum=0;
+  for(n=5;n>0;n--)
+  {
+     for(j=1;j<=n;j++)
+     {
+           f*=j;
+     }
+     f=f/n ;
+     sum+=f;
+     f=1;
+  }
+  return sum;
+}
+int main()
+{
+  int n;
+  printf("%d",fact(n));
+  getch();
+}
