baa9ffcb01624790cf5df55a627e9808b0bb3653
Hannavsleeba <62882708+Hannavsleeba@users.noreply.github.com>
2020-10-02 15:31:25 +0530

    Update Assignment 2a.c

--- Assignment 2a.c
+++ Assignment 2a.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-void main()
+int main()
 {
   char a[]="Athulya",b[]="St joseph HSS";
   int age=18,marks=100;
-  printf("My name is  %s\n",a);
-    printf("School is %s\n",b);
-    printf("Age is %d\n",age);
-  printf("Marks obtained is %d\n",marks);
+   printf("My name is  %s\n",a);
+   printf("I am studying at %s\n",b);
+   printf("Age is %d\n",age);
+   printf("Marks obtained is %d\n",marks);
+   return 0;
   
 } 
